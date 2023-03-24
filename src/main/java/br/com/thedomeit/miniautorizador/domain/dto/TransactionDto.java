3617c9e202802fc438c1bc6b5a8cb8c8efd8c6a4
thedomeContato <thedome.contato@yahoo.com.br>
2023-03-24 18:43:25 -0300

    Atualizacao nos Domains e Criação SwaggerConfig

--- src/main/java/br/com/thedomeit/miniautorizador/domain/dto/TransactionDto.java
+++ src/main/java/br/com/thedomeit/miniautorizador/domain/dto/TransactionDto.java
@@ -22,18 +22,18 @@ import lombok.ToString;
 @ToString
 public class TransactionDto {
 
-	@NotBlank(message = "cardNumber is necessary")
-    @Size(min = 16, message = "The card must have 16 numbers")
-    @Size(max = 16, message = "The card must have 16 numbers")
-    @Schema(description = "Number of card", example = "1234567890123456", required = true)
-	private String cardNumber;                         
+	@NotBlank(message = "Numero do Cartão é necessário")
+    @Size(min = 16, message = "Valor mínimo de 16 números")
+    @Size(max = 16, message = "Valor mínimo de 16 números")
+    @Schema(description = "Numero do Cartão", example = "1234567890123456", required = true)
+	private String numeroCartao;                         
 
-	@NotBlank(message = "Description Expense is necessary")
-	@Schema(description = "Password of card", example = "1234", required = true)
-	private String password;
+	@NotBlank(message = "Senha é necessária")
+	@Schema(description = "Senha do Cartão", example = "1234", required = true)
+	private String senhaCartao;
 
 	@DecimalMin(value = "0.0", inclusive = false)
 	@Digits(integer=5, fraction=2)
-	@Schema(description = "Value of Transaction", example = "12.34", required = true)
-	private BigDecimal valueTransaction;
+	@Schema(description = "Valor da Transacao", example = "12.34", required = true)
+	private BigDecimal valor;
 }
