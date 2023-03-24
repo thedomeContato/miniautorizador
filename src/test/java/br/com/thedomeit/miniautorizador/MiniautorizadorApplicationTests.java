97ed843e9436dd5db9190e079088f45d1bcbc568
thedomeContato <thedome.contato@yahoo.com.br>
2023-03-24 17:14:44 -0300

    Initial
    Initial

---
+++ src/test/java/br/com/thedomeit/miniautorizador/MiniautorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thedomeit.miniautorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniautorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
